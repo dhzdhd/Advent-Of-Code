c6969c17ef64ab3c5f32fca1f43747b071e32c7b
dhzdhd <ragas5432100@gmail.com>
2023-12-04 23:50:56 +0530

    Add partially complete 3

--- 2023/3/main.py
+++ 2023/3/main.py
@@ -1,15 +1,18 @@
 import functools as ft
 import itertools as it
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Gear:
+    value: str
+    pos: (int, int)
+
 
 with open("2023/3/input.txt") as f:
     raw: list[str] = list(map(lambda x: x.replace("\n", ""), f.readlines()))
     width = len(raw[0])
     height = len(raw)
-
-    # full = ""
-    # for i in raw:
-    #     full += i
-    # print(set(full))
 
     j = 0
     res = 0
@@ -52,9 +55,58 @@ with open("2023/3/input.txt") as f:
                 j += 1
 
             if flag:
-                print(num)
                 res += int(num)
                 flag = False
                 num = ""
             j += 1
+    # print(res)
+
+    objs: [Gear] = []
+    for i in range(height):
+        j = 0
+        while j < width:
+            buffer = ""
+
+            if raw[i][j] == "*":
+                buffer += raw[i][j]
+            else:
+                while raw[i][j].isdigit() and raw[i][j] != ".":
+                    buffer += raw[i][j]
+
+                    if j == width - 1:
+                        break
+                    j += 1
+
+            if buffer != "":
+                objs.append(Gear(buffer, (j, i)))
+            j += 1
+
+    stars = list(filter(lambda x: x.value == "*", objs))
+    gears = list(filter(lambda x: x.value != "*", objs))
+
+    res = 0
+    for i in stars:
+        couple = []
+        pos = i.pos
+
+        for j in gears:
+            if j.pos[1] - 1 <= pos[1] <= j.pos[1] + 1:
+                start = j.pos[0] - len(j.value)
+                end = j.pos[0]
+
+                if start - 1 <= pos[0] <= start + 1 or end - 1 <= pos[0] <= end + 1:
+                    couple.append(j)
+
+                # checks for top and bottom just fine but not on same line
+                # if (j.pos[0] - len(j.value) - 1) <= pos[0] <= (j.pos[0] + 1):
+                #     couple.append(j)
+                # elif j.pos[1] == pos[1] and j.pos[0] <= pos[0] <= j.pos[0]:
+                #     couple.append(j)
+        if len(couple) == 2:
+            res += int(couple[0].value) * int(couple[1].value)
+            print(couple)
+
+    # for i in objs:
+    #     print(i)
+
     print(res)
