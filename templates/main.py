57ad138488798c8e89303e57d55ff1f46840259d
dhzdhd <ragas5432100@gmail.com>
2023-12-09 19:49:16 +0530

    Add python template

---
+++ templates/main.py
@@ -0,0 +1,15 @@
+import itertools as it
+import functools as ft
+import math
+from functools import reduce
+from itertools import (
+    combinations,
+    combinations_with_replacement,
+    permutations,
+    product,
+    pairwise,
+    repeat,
+)
+
+with open("202X/X/input.txt") as f:
+    raw = f.readlines()
